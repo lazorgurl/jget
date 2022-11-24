b85121be8833b550d7204b257fe962c12b850781
Julia McGhee <julia.eloise@pm.me>
2022-11-23 19:17:02 -0800

    allow body and headers to be optional

--- README.md
+++ README.md
@@ -1,17 +1,22 @@
 # jget
 
 ```bash
-$ echo '{"url": "http://httpbin.org/ip", "method": "GET", "body": "", "headers": {}}' | jget | jq .
+$ jo url=https://httpbin.org/ip method=GET | jget | jq '.body | fromjson'
+{
+  "origin": "172.92.128.126"
+}
+$ jo url=https://httpbin.org/ip method=GET | jget | jq .
 {
   "status": 200,
+  "body": "{\n  \"origin\": \"172.92.128.126\"\n}\n",
   "headers": {
-    "content-length": "33",
+    "date": "Thu, 24 Nov 2022 03:16:09 GMT",
+    "connection": "keep-alive",
+    "server": "gunicorn/19.9.0",
+    "access-control-allow-credentials": "true",
     "access-control-allow-origin": "*",
     "content-type": "application/json",
-    "server": "gunicorn/19.9.0",
-    "connection": "keep-alive",
-    "access-control-allow-credentials": "true",
-    "date": "Wed, 23 Nov 2022 23:35:52 GMT"
+    "content-length": "33"
   }
 }
 ```
--- main.rs
+++ main.rs
@@ -1,57 +1,62 @@
 use reqwest;
 use reqwest::header::{HeaderMap, HeaderName, HeaderValue};
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 use serde_json;
 use std::collections::HashMap;
 use std::io::stdin;
 use std::str::FromStr;
 
 #[derive(Serialize, Deserialize, Debug)]
-struct RequestIn{
+struct RequestIn {
     url: String,
     method: String,
+    #[serde(default = "String::new")]
     body: String,
-    headers: HashMap<String, String>
+    #[serde(default = "HashMap::new")]
+    headers: HashMap<String, String>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct ResponsetOut{
+struct ResponsetOut {
     status: u16,
-    // body: String,
-    headers: HashMap<String, String>
+    body: String,
+    headers: HashMap<String, String>,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let spec: RequestIn = serde_json::from_reader(stdin())?;
+
     let mut header_map = HeaderMap::new();
-    spec.headers.iter().for_each(
-        |(k, v)| {
-            header_map.append(
-                HeaderName::from_str(k.as_str()).unwrap(), 
-                HeaderValue::from_str(v.as_str()).unwrap());
-        }
-    );
+    spec.headers.iter().for_each(|(k, v)| {
+        header_map.append(
+            HeaderName::from_str(k.as_str()).unwrap(),
+            HeaderValue::from_str(v.as_str()).unwrap(),
+        );
+    });
 
     let client = reqwest::Client::new();
-    
-    let req = client.request(
-        reqwest::Method::from_str(&spec.method)?, 
-        reqwest::Url::parse(&spec.url)?)
-    .headers(header_map)
-    .body(spec.body)
-    .build()?;
+
+    let req = client
+        .request(
+            reqwest::Method::from_str(&spec.method)?,
+            reqwest::Url::parse(&spec.url)?,
+        )
+        .body(spec.body)
+        .headers(header_map)
+        .build()?;
 
     let resp = client.execute(req).await?;
-    let header_out = resp.headers().iter()
-    .map(|(k, v)| {
-        (k.to_string(), v.to_str().unwrap().to_string())
-    }).collect::<HashMap<String, String>>();
+    let header_out = resp
+        .headers()
+        .iter()
+        .map(|(k, v)| (k.to_string(), v.to_str().unwrap().to_string()))
+        .collect::<HashMap<String, String>>();
 
-  let out: ResponsetOut = ResponsetOut {
+    let out: ResponsetOut = ResponsetOut {
         status: resp.status().as_u16(),
-        headers: header_out, 
-        // body: resp.json().await?
+        headers: header_out,
+        body: resp.text().await?,
     };
 
     println!("{}", serde_json::to_string(&out)?);
